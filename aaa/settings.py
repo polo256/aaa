f5b4c66b19e0055cf3e43d7490a6ca1586464eb1
polo256 <p_kabagambe@yahoo.com>
2017-03-21 09:46:46 +0300

    Update settings.py

--- aaa/settings.py
+++ aaa/settings.py
@@ -94,14 +94,14 @@ WSGI_APPLICATION = 'aaa.wsgi.application'
 
 DATABASES = {
     'default': {
-       #'ENGINE': 'django.db.backends.sqlite3',
-       #'NAME': (os.path.join(BASE_DIR, 'db.sqlite3')) ,
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'secretariat',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': (os.path.join(BASE_DIR, 'db.sqlite3')) ,
+       # 'ENGINE': 'django.db.backends.mysql',
+       # 'NAME': 'secretariat',
+       # 'USER': 'root',
+       # 'PASSWORD': '',
+       # 'HOST': 'localhost',
+       # 'PORT': '',
         
     }
 }
