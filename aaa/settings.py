986b350c8a9b68b03c210c850f4be1cd4d2800bf
polo256 <p_kabagambe@yahoo.com>
2017-03-21 10:30:53 +0300

    Update settings.py

--- aaa/settings.py
+++ aaa/settings.py
@@ -97,11 +97,11 @@ DATABASES = {
        #'ENGINE': 'django.db.backends.sqlite3',
        #'NAME': (os.path.join(BASE_DIR, 'db.sqlite3')) ,
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'secretariat',
-        'USER': 'aaacadem_pk',
-        'PASSWORD': 'Max1n4',
-        'HOST': 'username.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
+        'NAME': 'polo256$secretariat',
+        'USER': 'polo256',
+        'PASSWORD': 'Maxine123',
+        'HOST': 'polo256.mysql.pythonanywhere-services.com',
+        #'PORT': '3306',
         
     }
 }
